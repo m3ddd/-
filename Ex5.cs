ce00cede2253bb6a5017b0d1a586fb5a76fa5bf1
Безолюк Антон <fokycnik.anton@mail.ru>
2021-05-03 16:34:12 +0300

    Пятое задание

---
+++ Ex5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+            // б) Сделать задание, только вывод организовать в центре экрана.
+
+            Console.WriteLine("Ввдедите свое имя: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("Введите свою фамилию: ");
+            string b = Console.ReadLine();
+            Console.WriteLine("Введите город проживания: ");
+            string c = Console.ReadLine();
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+            Console.WriteLine($"Имя: {a} Фамилия: {b} Город проживания: {c}");
+
+
+
+        }
+    }
+}
